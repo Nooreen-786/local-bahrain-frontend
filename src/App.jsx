b345b7ee6b0e8c9f51d6598b591857718e016550
User Name <nooreenkhatoon786@gmail.com>
2025-08-26 16:14:36 +0300

    Initial frontend commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,24 +8,27 @@ import PlaceListPage from './pages/PlaceListPage';
 import PlaceDetailPage from './pages/PlaceDetailPage';
 import PlaceFormPage from './pages/PlaceFormPage';
 import SignIn from './pages/SignIn';
+import Register from './pages/Register'; 
+
 
 import RestaurantListPage from './pages/RestaurantListPage';
 import RestaurantFormPage from './pages/RestaurantFormPage';
 import RestaurantDetailPage from './pages/RestaurantDetailPage';
 import Account from './pages/Account';  
+
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<SignIn />} />
+        <Route path="/register" element={<Register />} /> 
 
         <Route path="/places" element={<PlaceListPage />} />
         <Route path="/places/:id" element={<PlaceDetailPage />} />
         <Route path="/restaurants" element={<RestaurantListPage />} />
         <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
 
-       
         <Route element={<PrivateRoute />}>
           <Route path="/places/new" element={<PlaceFormPage />} />
           <Route path="/places/:id/edit" element={<PlaceFormPage />} />
